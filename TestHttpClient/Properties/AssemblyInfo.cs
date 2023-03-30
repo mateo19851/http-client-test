318298eecfdf0ea4260c3757310ee42f3b18a18f
Mateusz Malik <mateusz.malik@medius.com>
2023-03-30 07:08:18 +0200

    Tests for httpclient

---
+++ TestHttpClient/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestHttpClient")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestHttpClient")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c459b165-2644-49c5-afb3-db2e3855d3f3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestHttpClient/UnitTest1.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+
+namespace TestHttpClient
+{
+    [TestFixture]
+    public class HttpClientApprachTests
+    {
+        private static readonly string WebApiForecast = $"https://localhost:{ApplicationPort}/WeatherForecast";
+        private const int ApplicationPort = 7187;
+        private IHttpClientFactory _httpClientFactoryAsExtension;
+        private List<TcpConnectionInformation> _initialConnectionsCnt;
+
+        private Func<TcpConnectionInformation, bool> ActiveTcpConnectionsPredicate = x =>
+        {
+            return x.RemoteEndPoint.Port == ApplicationPort;
+        };
+
+        [SetUp]
+        public void Initialize()
+        {
+            var serviceProvider = new ServiceCollection().AddHttpClient().BuildServiceProvider();
+            _httpClientFactoryAsExtension = serviceProvider.GetService<IHttpClientFactory>();
+            _initialConnectionsCnt = ShowActiveTcpConnections().Where(ActiveTcpConnectionsPredicate).ToList();
+        }
+
+        [Test]
+        public async Task ShouldNotLeaveUnwantedConnections_WhenHttpClientUsedDirectly()
+        {
+            // Arrange
+            var listOfResponse = new List<HttpResponseMessage>();
+
+            // Act
+            for (var i = 100; i > 0; i--)
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    listOfResponse.Add(await httpClient.GetAsync(WebApiForecast));
+                }
+            }
+
+            var tcpConnections = ShowActiveTcpConnections().Where(ActiveTcpConnectionsPredicate).ToList();
+
+            // Assert
+            var connectionDiff = tcpConnections.Count() - _initialConnectionsCnt.Count();
+            Assert.IsTrue(listOfResponse.All(x => x.IsSuccessStatusCode));
+            Assert.That(connectionDiff, Is.EqualTo(1));
+        }
+
+
+        [Test]
+        public async Task ShouldNotLeaveUnwantedConnections_WhenHttpClientFromHttpClientFactory()
+        {
+            // Arrange
+            var listOfResponse = new List<HttpResponseMessage>();
+
+            // Act
+            for (var i = 100; i > 0; i--)
+            {
+                using (var httpClient = HttpClientFactory.Create())
+                {
+                    listOfResponse.Add(await httpClient.GetAsync(WebApiForecast));
+                }
+            }
+
+            var tcpConnections = ShowActiveTcpConnections().Where(ActiveTcpConnectionsPredicate).ToList();
+
+            // Assert
+            var connectionDiff = tcpConnections.Count() - _initialConnectionsCnt.Count();
+            Assert.IsTrue(listOfResponse.All(x => x.IsSuccessStatusCode));
+            Assert.That(connectionDiff, Is.EqualTo(1));
+        }
+
+        [Test]
+        public async Task ShouldNotLeaveUnwantedConnections_WhenHttpClientFromIHttpClientFactory()
+        {
+            // Arrange
+            var listOfResponse = new List<HttpResponseMessage>();
+
+            // Act
+            for(var i = 100; i > 0; i--)
+            {
+                using (var httpClient = _httpClientFactoryAsExtension.CreateClient())
+                {
+                    listOfResponse.Add(await httpClient.GetAsync(WebApiForecast));
+                }
+            }
+
+            var tcpConnections = ShowActiveTcpConnections().Where(ActiveTcpConnectionsPredicate).ToList();
+
+            // Assert
+            var connectionDiff = tcpConnections.Count() - _initialConnectionsCnt.Count();
+            var someConns = ShowActiveTcpConnections().Where(x => x.RemoteEndPoint.Port == ApplicationPort);
+            Assert.IsTrue(listOfResponse.All(x => x.IsSuccessStatusCode));
+            Assert.That(connectionDiff, Is.EqualTo(1));
+        }
+
+        public static IEnumerable<TcpConnectionInformation> ShowActiveTcpConnections()
+        {
+           IPGlobalProperties properties = IPGlobalProperties.GetIPGlobalProperties();
+           return  properties.GetActiveTcpConnections();
+        }
+    }
+}
